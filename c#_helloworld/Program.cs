d2f8e7139e7b2c1cc3950af3904225540a6be5bd
Ahmad Amzah <s101620674@gmail.com>
2019-03-18 22:21:19 +1100

    Hell

---
+++ c#_helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c__helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yeet");
+        }
+    }
+}
