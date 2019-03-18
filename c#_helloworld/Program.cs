24ce9d30171e7002b5deeb1623c90ce9029123d4
Ahmad Amzah <s101620674@gmail.com>
2019-03-18 21:55:41 +1100

    Fixes #31

---
+++ c#_helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c__helloworld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
