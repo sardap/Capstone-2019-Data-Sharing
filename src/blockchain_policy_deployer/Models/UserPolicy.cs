9698fafb0fe4473b67a4f1ff0147bf1420df576a
Shane <vincent.shane101@gmail.com>
2019-05-09 20:37:20 +1000

    Initial commit. Successful incoming data collection and request to validator.

---
+++ src/blockchain_policy_deployer/Models/UserPolicy.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blockchain_policy_deployer.Models
+{
+    public class UserPolicy
+    {
+        public JObject JsonPolicy { get; set; }
+        public string DataSubjectWalletID { get; set; }
+    }
+}
---
+++ src/blockchain_policy_deployer/Models/UserPolicyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace blockchain_policy_deployer.Models
+{
+    public class UserPolicyContext :  DbContext
+    {
+        public UserPolicyContext(DbContextOptions<UserPolicyContext> options)
+            : base(options)
+        {
+        }
+
+        public UserPolicy incomingPolicy; 
+
+        public DbSet<UserPolicy> UserPolicies { get; set; }
+    }
+}
