b28e685fef66f6fe228dd3c55328298a966d3baa
sardap <sardap@users.noreply.github.com>
2019-05-24 22:12:35 +1000

    It needs to match the json to work
    We need to make an exception for now in the code style

--- src/BCF/BCPolicyValidator/policy_validator/Models/policy.cs
+++ src/BCF/BCPolicyValidator/policy_validator/Models/policy.cs
@@ -5,7 +5,7 @@ namespace Policy_Validator.Models
 {
     public class PolicyModel
     {
-        public List<int> ExcludedCategories {get; set;}
+        public List<int> Excluded_categories {get; set;}
         public int? Min_price {get; set;}
         public Time Time_period {get; set;}
         public string Data_type {get; set;}
