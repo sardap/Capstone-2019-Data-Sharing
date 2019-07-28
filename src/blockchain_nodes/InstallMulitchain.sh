aa6b5b867bbbd4bb207e7885e69f574e0c1e1818
sardap <sardap@users.noreply.github.com>
2019-07-28 15:17:18 +1000

    Removed whitespace

--- src/blockchain_nodes/InstallMulitchain.sh
+++ src/blockchain_nodes/InstallMulitchain.sh
@@ -1,19 +1,11 @@
 #bin/bash
-
 cd ~
-
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
-
 sudo apt-get update
-
 sudo apt install -y net-tools software-properties-common build-essential libtool autotools-dev automake pkg-config libssl-dev git python python-pip git libdb4.8-dev libdb4.8++-dev libboost1.65-all-dev autoconf g++ make openssl libcurl4-openssl-dev autogen libsasl2-dev
-
 sudo pip install pathlib2
-
 sudo apt upgrade -y
-
 cd ~
-
 git clone https://github.com/MultiChain/multichain.git
 cd multichain
 set MULTICHAIN_HOME=$(pwd)
