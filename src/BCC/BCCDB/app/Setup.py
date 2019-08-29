1368352755367d445095e22fca11d61c3fd74b86
sardap <pfsarda23@gmail.com>
2019-08-29 16:34:22 +1000

    added broker table and defualt values

--- src/BCC/BCCDB/app/Setup.py
+++ src/BCC/BCCDB/app/Setup.py
@@ -23,12 +23,29 @@ def main():
 	database = os.environ['MYSQL_DATABASE']
 	cur.execute("CREATE DATABASE IF NOT EXISTS " + database + ";")
 	cur.execute("USE " + database + ";")
+	cur.execute("CREATE TABLE IF NOT EXISTS Broker ( \
+		ID int NOT NULL AUTO_INCREMENT, \
+		DataBrokerAPIKey varchar(255) NOT NULL, \
+		BrokerName varchar(255), \
+		PRIMARY KEY (ID) \
+	) ENGINE = InnoDB;")
 	cur.execute("CREATE TABLE IF NOT EXISTS Policy ( \
 		OffChainPolicyID varchar(255) NOT NULL, \
 		APIAddress varchar(255) NOT NULL, \
 		OnchainAddress varchar(255) NOT NULL, \
-		PRIMARY KEY (OffChainPolicyID) \
-	);")
+		DataBrokerID int NOT NULL, \
+		PRIMARY KEY (OffChainPolicyID, DataBrokerID), \
+		FOREIGN KEY (DataBrokerID) REFERENCES Broker(ID) \
+	) ENGINE = InnoDB;")
+
+	if(True or ('SET_DEFAULT_BROKER' in os.environ and os.environ['SET_DEFAULT_BROKER'].lower() == "true")):
+		cur.execute("INSERT INTO Broker(DataBrokerAPIKey, BrokerName) VALUES('broker1', 'It Just Works');")
+		cur.execute("INSERT INTO Broker(DataBrokerAPIKey, BrokerName) VALUES('broker2', 'No Worries INC');")
+		cur.execute("INSERT INTO Broker(DataBrokerAPIKey) VALUES('broker3');")
+
+	mydb.commit()
+	cur.close()
+	mydb.close()
 
 	#Work around so it doesn't leave exit signal which kills the containtor
 	while(True):
