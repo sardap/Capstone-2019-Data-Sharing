e36b48189e5c4066719702292de0c96174ad1088
Andrew <andydavis0@hotmail.com>
2019-10-01 15:18:28 +1000

    rename example data file from data.json example_data.json

--- src/BCC/Policy_deactivator/run.sh
+++ src/BCC/Policy_deactivator/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node index.js 127.0.0.1:3000 some_username some_password wallet_xyz location_foobar data.json
+node index.js 127.0.0.1:3000 some_username some_password wallet_xyz location_foobar example_data.json
