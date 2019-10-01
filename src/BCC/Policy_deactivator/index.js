31e4c370646ff6d76d50a3586f10eb2006860d90
Andrew <andydavis0@hotmail.com>
2019-10-01 15:16:36 +1000

    update of policy deactivator.  POST looks well formed, need to test against chain

--- src/BCC/Policy_deactivator/index.js
+++ src/BCC/Policy_deactivator/index.js
@@ -1,26 +1,22 @@
-//what needs to be included to deactivate a policy
-//send command to publish new state to the stream
-//we need
-    //address of the blockchain
-    //need a wallet address
-        //each policy has its own stream
-    //need a location
-    //json representing the new state
-    //{active: "false"}
-    //assumption is the append will change the policy to only {active: "false"}
+#!/usr/bin/nodejs
 
-    //Assuming we need wallet ID, where is it in the RPC Post?
-    //The user is running a node, how do they get the streamid/location
+
 
 //example data
 //{"method":"publishfrom","params":["<from address / wallet id>","<stream1/location">,"policy", <data>],"chain_name":"Mainchain"}
     //all calls are POSTS, the method is the method called on the chain
+
+
+//import filesystem object
+//import axios a http client package
 const axios = require('axios');
+const fs = require('fs');
 
 
-function post_data(wallet_id, location, data)
+//Make the HTTP post to the multichain RPC
+function post_data(address, username, password, wallet_id, location, data)
 {
-    axios.post('http://localhost:3000/post', {
+    axios.post("http://" + address + "/post", {
         "method":"publishfrom",
         "params":[
             wallet_id,
@@ -30,18 +26,68 @@ function post_data(wallet_id, location, data)
         ,"chain_name":"Mainchain"}
         ,{
     auth: {
-        username: "some_username",
-        password: "some_password"
+        username: username,
+        password: password
       }
   }).then(function (response) {
     console.log(response.config.data);
   }).catch(function (error) {
-    console.log(error);
+    console.error(error);
   });
 }
 
+function help(message, code)
+{
+    if(!code){
+        code = 1;
+    }
+
+    if(code == 0){
+        out = console.log;
+    } else {
+        out = console.error;
+    }
+    
+    if(message){
+        out(message);
+    }
+
+    out("Usage:");
+    out("\t$deactivator <address:port> <username> <password> <wallet_id> <location> <data_filename>");
+    out("example:")
+    out("\t$deactivator 127.0.0.1:3000 some_username abc123 ABcDXyZ ffg222323323232 data.json");
+
+    process.exit(code);
+}
+
 function main(){
-    post_data("wallet_id", "xyz_location", {active: "false"});
+    let args = process.argv.slice(1);
+    
+    //check for 
+    if (!args[1]) {
+        help("incorrect number of arguments", 1);
+    } else if (args[1] == '-h' || args[1] == '-?'|| args[1] == "--help") {
+        help(null, 0);
+    }
+
+    if(args.length != 7){
+        help("incorrect number of arguments", 1);
+    }
+
+    //get the cli arguments
+    //ideally some validation would be good
+    let address = args[1];
+    let username = args[2];
+    let password = args[3];
+    let wallet_id = args[4];
+    let location = args[5];
+    let filename = args[6];
+
+    let data_string = fs.readFileSync(filename, 'utf8');
+    let data = JSON.parse(data_string);
+
+    post_data(address, username, password, wallet_id, location, data);
+
 }
 
 main();
