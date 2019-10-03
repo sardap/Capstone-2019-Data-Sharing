29f0068eeb2450f07bb7c8d5d0535e553ae2fe3b
Nhi <nhivn.dev@gmail.com>
2019-10-03 23:06:22 +1000

    Added Data Sharing Policy API

---
+++ src/data_broker/front_end/DataBroker/Controllers/PolicyController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using DataBroker.Data;
+using DataBroker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace DataBroker.Controllers
+{
+	[ApiController]
+	public class PolicyController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+		public PolicyController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet("/api/GetAllPolicies")]
+		public IActionResult GetAll()
+		{
+			var user = _context.ApplicationUsers.SingleOrDefault(z => z.Email.Equals(User.Identity.Name));
+			if (user == null) return Json(new {success = false, message = "Invalid user"});
+
+			var policies = _context.DataSharingPolicies.AsEnumerable().Where(z => z.UserId == user.Id).ToArray();
+			return Json(new {success = true, data = policies});
+		}
+
+		public class DspPoco
+		{
+			[JsonProperty("excluded")]
+			public string ExcludedBuyers { get; set; }
+			[JsonProperty("start")]
+			public string Start { get; set; }
+			[JsonProperty("end")]
+			public string End { get; set; }
+			[JsonProperty("min_price")]
+			public decimal MinPrice { get; set; }
+			[JsonProperty("active")]
+			public bool Active { get; set; }
+		}
+
+		[HttpPost("/api/AddPolicy")]
+		public IActionResult Add(DspPoco policy)
+		{
+			var user = _context.ApplicationUsers.SingleOrDefault(z => z.Email.Equals(User.Identity.Name));
+			if (user == null) return Json(new {success = false, message = "Invalid user"});
+
+			var newPolicy = new DataSharingPolicy
+			{
+				UserId = user.Id,
+				ExcludedBuyers = policy.ExcludedBuyers,
+				Start = DateTime.Parse(policy.Start),
+				End = DateTime.Parse(policy.End),
+				MinPrice = policy.MinPrice,
+				Active = policy.Active
+			};
+			_context.DataSharingPolicies.Add(newPolicy);
+			_context.SaveChanges();
+			
+			// TODO: Send request to policy drop off point
+			
+			return Json(new {success = true, message = "Successfully added new policy!"});
+		}
+	}
+}
--- src/data_broker/front_end/DataBroker/Models/DataSharingPolicy.cs
+++ src/data_broker/front_end/DataBroker/Models/DataSharingPolicy.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DataBroker.Models
 {
     public class DataSharingPolicy
     {
+        [Key]
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+        [JsonIgnore]
+        public Guid UserId { get; set; }
+        [JsonProperty("excluded")]
+        public string ExcludedBuyers { get; set; }
+        [JsonProperty("start")]
+        public DateTime Start { get; set; }
+        [JsonProperty("end")]
+        public DateTime End { get; set; }
+        [JsonProperty("min_price")]
+        public decimal MinPrice { get; set; }
+        [JsonProperty("active")]
+        public bool Active { get; set; }
     }
 }
