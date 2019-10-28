894d489e5a36ef252d17cb491da4deafd17ac8f3
Nhi <nhivn.dev@gmail.com>
2019-10-28 22:54:50 +1100

    Clear warning of not specified SQL data type

--- src/data_broker/front_end/DataBroker/Models/DataSharingPolicy.cs
+++ src/data_broker/front_end/DataBroker/Models/DataSharingPolicy.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +23,7 @@ namespace DataBroker.Models
         [JsonProperty("end")]
         public DateTime End { get; set; }
         [JsonProperty("min_price")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal MinPrice { get; set; }
         [JsonProperty("active")]
         public bool Active { get; set; }
